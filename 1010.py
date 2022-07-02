fe967e250a407aadbde84d4e995b5c9dd768b7d6
victornico15 <vnespindola15@gmail.com>
2022-07-02 19:58:41 -0300

    Commit 02/07
    Inserção dos programaas executados da semana

--- 1010.py
+++ 1010.py
@@ -1,4 +1,3 @@
-
 linha1 = input().split(" ")
 linha2 = input().split(" ")
 
@@ -7,4 +6,4 @@ cod2, qtde2, valor2 = linha2
 
 total = (int(qtde1) * float(valor1)) + (int(qtde2) * float(valor2))
 
-print("VALOR A PAGAR: R$ %0.2f" % total)
+print("VALOR A PAGAR: R$ %0.2f" %total)
